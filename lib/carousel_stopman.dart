56c81d810e4b0b944d3faead2f1d021c7f8958c6
micknice <mikemccullagh@outlook.com>
2023-06-08 09:14:51 +0100

    stopman  carousel item further progress

--- lib/carousel_stopman.dart
+++ lib/carousel_stopman.dart
@@ -32,88 +32,144 @@ class _CarouselStopmanState extends State<CarouselStopman> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 400,
-              width: 360,
-              child: Stack(
-                children: [
-                  FutureBuilder(
-                    future: _initializeVideoPlayerFuture,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        _controller.play();
-                        return ClipRect(
-                          child: OverflowBox(
-                            alignment: Alignment.center,
-                            child: SizedBox(
-                              width: double.infinity,
-                              height: double.infinity,
-                              child: AspectRatio(
-                                aspectRatio: _controller.value.aspectRatio,
-                                child: VideoPlayer(_controller),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 23,
+              ),
+              SizedBox(
+                height: 400,
+                width: 360,
+                child: Stack(
+                  children: [
+                    FutureBuilder(
+                      future: _initializeVideoPlayerFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          _controller.play();
+                          return ClipRect(
+                            child: OverflowBox(
+                              alignment: Alignment.center,
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: AspectRatio(
+                                  aspectRatio: _controller.value.aspectRatio,
+                                  child: VideoPlayer(_controller),
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  ),
-                  const Positioned.fill(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Text(
-                          'STOPMAN',
-                          style: TextStyle(
-                            fontSize: 60,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                          );
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      },
+                    ),
+                    const Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            'STOPMAN',
+                            style: TextStyle(
+                              fontSize: 60,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const Align(
-                      alignment: Alignment.centerRight,
-                      child: SizedBox(
-                          width: 4,
-                          height: 516,
-                          child: DecoratedBox(
-                              decoration: BoxDecoration(color: Colors.black)))),
-                  const Align(
-                      alignment: Alignment.centerLeft,
-                      child: SizedBox(
-                          width: 4,
-                          height: 516,
-                          child: DecoratedBox(
-                              decoration: BoxDecoration(color: Colors.black)))),
-                ],
-              ),
-            ),
-            const Padding(
-              
-              padding: EdgeInsets.all(8.0),
-              child: Stack(children: [
-                
-                Center(
-                  child: SizedBox(
-                    height: 100,
-                    width: 360,
-                    child: DecoratedBox(
-                        decoration: BoxDecoration(color: Colors.black)),
-                  ),
+                    const Align(
+                        alignment: Alignment.centerRight,
+                        child: SizedBox(
+                            width: 4,
+                            height: 516,
+                            child: DecoratedBox(
+                                decoration: BoxDecoration(color: Colors.black)))),
+                    const Align(
+                        alignment: Alignment.centerLeft,
+                        child: SizedBox(
+                            width: 4,
+                            height: 516,
+                            child: DecoratedBox(
+                                decoration: BoxDecoration(color: Colors.black)))),
+                  ],
                 ),
-                Text('SHOP THE TREND',style: TextStyle(color: Colors.white),)
-              ]),
-            )
-          ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(4.0),
+                child: Stack(
+                  alignment: Alignment.center,
+                  fit: StackFit.passthrough,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        height: 100,
+                        width: 360,
+                        child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.black)),
+                      ),
+                    ),
+                    Center(
+                        child: Text(
+                      'SHOP THE TREND',
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 35),
+                    ))
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(4.0),
+                child: Stack(
+                  alignment: Alignment.center,
+                  fit: StackFit.passthrough,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        height: 100,
+                        width: 360,
+                        child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.black)),
+                      ),
+                    ),
+                    Center(
+                        child: Text(
+                      'NEW SWEG',
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40),
+                    ))
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(4.0),
+                child: Stack(
+                  alignment: Alignment.center,
+                  fit: StackFit.passthrough,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        height: 100,
+                        width: 360,
+                        child: DecoratedBox(
+                            decoration: BoxDecoration(color: Colors.black)),
+                      ),
+                    ),
+                    Center(
+                        child: Text(
+                      'MOST WANTED',
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 38),
+                    ))
+                  ],
+                ),
+              ),
+              
+            ],
+          ),
         ),
       ),
     );
